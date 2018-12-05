a256ceb316fe1d837aa64acfe224e15885c92025
Riff Ventura <riff.ventura@gmail.com>
2018-12-05 21:27:38 +0100

    basis shapes rectangle path and arc

--- js/lab1.js
+++ js/lab1.js
@@ -1,8 +1,36 @@
 function drawScreen(context) {
-    context.fillStyle = '#aaaaaa';
-    context.fillRect(0, 0, 200, 200);
+    //draws rectangles
     context.fillStyle = '#000000';
-    context.font = '20px _sans';
-    context.textBaseline = 'top';
-    context.fillText("Canvas!", 0, 0);
+    context.strokeStyle = '#ff00ff';
+    context.lineWidth = 2;
+    context.fillRect(10, 10, 40, 40);
+    context.strokeRect(0, 0, 60, 60);
+    context.clearRect(20, 20, 20, 20);
+
+    //path strokes
+    context.strokeStyle = "black";
+    context.lineWidth = 10;
+    context.lineCap = 'square';
+    context.beginPath();
+    context.moveTo(200, 90);
+    context.lineTo(300, 90);
+    context.lineTo(300, 150);
+    context.stroke();
+
+    //path triangle
+    context.fillStyle = '#fff000';
+    context.moveTo(400, 300);
+    context.lineTo(400, 150);
+    context.lineTo(300, 225);
+    context.lineTo(400, 300);
+    context.lineCap = 'round';
+    context.stroke();
+    context.closePath()
+
+    //arc
+    context.strokeStyle = '#ff00ff';
+    context.beginPath();
+    context.arc(400, 400, 100, (Math.PI / 180) * 0, (Math.PI / 180) * 240, false);
+    context.stroke();
+    context.closePath()
 }
