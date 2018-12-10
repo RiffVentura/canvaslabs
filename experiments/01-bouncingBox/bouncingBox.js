14dbc8e1318049c811b3d356555392348b483426
Riff Ventura <riff.ventura@gmail.com>
2018-12-10 19:23:54 +0100

    bouncing changes colors and box now has a hitbox

--- experiments/01-bouncingBox/bouncingBox.js
+++ experiments/01-bouncingBox/bouncingBox.js
@@ -59,35 +59,58 @@ function initBox() {
     let directionX = 1;
     let directionY = 1;
 
+    const colors = ['green','red','black','blue','indigo','orange'];
+    let colorIdx = -1;
+
     return {
         x: 0,
         y: 0,
         width: 32,
         height: 32,
+        radius: 16,
         color: 'red',
         speed: 2,
         update: function () {
+            let bounce = false;
+
             this.x = this.x + 1 * this.speed * directionX;
-            if (this.x > limits.right) {
+            if ( this.centerX() + this.radius > limits.right) {
                 directionX = -1;
+                bounce = true;
             }
-            if (this.x < limits.left) {
+            if (this.centerX() - this.radius < limits.left) {
                 directionX = 1;
+                bounce = true;
             }
             this.y = this.y + 1 * this.speed * directionY;
-            if (this.y > limits.down) {
+            if (this.centerY() + this.radius > limits.down) {
                 directionY = -1;
+                bounce = true;
             }
-            if (this.y < limits.top) {
+            if (this.centerY() - this.radius < limits.top) {
                 directionY = 1;
+                bounce = true;
+            }
+
+            if(bounce){
+                this.color = colors[++colorIdx % colors.length];
             }
         },
         render: function () {
             let ctx = mainApp.context;
             ctx.fillStyle = this.color;
             ctx.fillRect(this.x, this.y, this.width, this.height)
-        }
-        //TODO getCenter
-        //TODO randomColors onBounce
-    }
+            
+            
+            ctx.beginPath();
+            ctx.strokeStyle = 'white';
+            ctx.lineWidth = 2;
+            ctx.arc(this.centerX(), this.centerY(), 6, 0, (Math.PI / 180) * 360, false);
+            ctx.stroke();
+            ctx.closePath()
+
+        },
+        centerX: function() { return this.x + this.width/2 },
+        centerY: function() { return this.y + this.height/2 },
+    };
 }
